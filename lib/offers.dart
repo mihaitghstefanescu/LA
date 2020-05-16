f6a4c49ba8308915498144bc80a6027ceaf6e88c
Mike <mihai.t.gh.stefanescu@gmail.com>
2020-05-16 17:51:21 +0300

    Community and offers pages

--- lib/offers.dart
+++ lib/offers.dart
@@ -1,20 +1,162 @@
 import 'package:flutter/material.dart';
 
+class Place {
+  String imageUrl;
+  String name;
+  String country;
+
+  Place({this.imageUrl, this.name, this.country});
+}
+
 class OffersPage extends StatelessWidget {
   const OffersPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List wonders = [
+      Place(
+          imageUrl:
+              "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/Taj-Mahal.jpg",
+          name: "Taj Mahal",
+          country: "India"),
+      Place(
+          imageUrl:
+              "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/Christ-the-Redeemer.jpg",
+          name: "Christ the Redeemer",
+          country: "Brazil"),
+      Place(
+          imageUrl:
+              "https://d36tnp772eyphs.cloudfront.net/blogs/1/2016/03/petra-jordan9.jpg",
+          name: "Petra",
+          country: "Jordan"),
+      Place(
+          imageUrl:
+              "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/Great-Wall-of-China-view.jpg",
+          name: "The Great Wall of China",
+          country: "China"),
+      Place(
+          imageUrl:
+              "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/View-of-the-Colosseum.jpg",
+          name: "The Colosseum",
+          country: "Rome"),
+      Place(
+          imageUrl:
+              "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/Machu-Picchu-around-sunset.jpg",
+          name: "Machu Picchu",
+          country: "Peru"),
+      Place(
+          imageUrl:
+              "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/Chichen-Itza-at-night.jpg",
+          name: "Chichén Itzá",
+          country: "Mexico"),
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("View offers"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 25, 0, 15),
+            child: Center(
+              child: Text(
+                'Offers / KABN Kash',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 30,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Text(
+              'NEW FASHION OFFERS FOR YOU',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 15,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: wonders.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, index) {
+                return Card(
+                    child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.network(
+                        wonders[index].imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Text(wonders[index].name),
+                    Text(wonders[index].country),
+                  ],
+                ));
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Text(
+              'NEW TRAVEL OFFERS FOR YOU',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 15,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              // This next line does the trick.
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 160.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Text(
+              'BROWSE ALL RETAIL OFFERS',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 15,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('Lorem Ipsum'),
-          subtitle: Text('$index'),
-        );
-      }),
     );
   }
 }
